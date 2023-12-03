7aa64850d7bedcbba94ac355de5f57a097548288
Eugene Kaygorodov <e.kaygorodov@artsofte.ru>
2023-12-03 21:53:10 +0500

    server fix

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 /* eslint-disable */
 const express = require("express");
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("./dist"));
+
+app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname + '/dist/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
